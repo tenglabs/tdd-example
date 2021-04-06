4f3f41eef03335b660688f5b4374b5d5f19e02bb
tenglabs <tengoneotechnologies@gmail.com>
2021-04-06 20:06:14 +0300

    test

--- ner_client.py
+++ ner_client.py
@@ -1,9 +1,25 @@
-
+import spacy
 
 
 class NamedEntityClient:
-    def __init__(self):
-        pass
+
+    def __init__(self, model):
+
+        self.model = model
     
+
     def get_ents(self,sentence):
-        return {}
+
+        doc = self.model(sentence)
+        entities =[{'ent':ent.text, 'label': self.map_label(ent.label_) } for ent in doc.ents ]
+
+        return {'ents': entities, 'html':''}
+
+
+    @staticmethod
+    def map_label(label):
+        label_map = {
+            'PERSON': 'Person'
+        }
+
+        return label_map.get(label)
--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -1,12 +1,31 @@
 import unittest
 from ner_client import NamedEntityClient
-
+from test_doubles import NerModelTestDouble
 
 class TestNerClient(unittest.TestCase):
 
-    def test_get_ents_returns_dic_emp_inp(self):
 
-        ner = NamedEntityClient()
+    def test_get_ents_returns_dic_emp_str(self):
+        model = NerModelTestDouble('eng')
+        model.return_doc_ents([])
+        ner = NamedEntityClient(model)
         ents = ner.get_ents("")
-        self.assertIsInstance(ents,dict)
+        self.assertIsInstance(ents, dict)
 
+
+    def test_get_ents_ret_dic_nonempty(self):
+        model = NerModelTestDouble('eng')
+        model.return_doc_ents([])
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents("Data")
+        self.assertIsInstance(ents, dict)
+
+
+    def test_get_ents_g_s_PERSON_returned_serializes(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text':'Laurent Fressinet', 'label_':'PERSON'}]
+        model.return_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('')
+        expected_result = {'ents': [{'ent': 'Laurent Fressinet', 'label': 'Person'}], 'html': ""  }
+        self.assertListEqual(result['ents'], expected_result['ents'])
